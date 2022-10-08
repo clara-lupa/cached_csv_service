6e6f06a0c5a77b84dea30b387b3e8f56743bcd3d
cw <cwo@systemli.org>
2022-10-08 15:48:28 +0200

    create unit tests for get_value function

--- test.py
+++ test.py
@@ -1,9 +1,41 @@
+from cgi import test
+from typing import Dict
 import unittest
 from main import get_values
+import json
 
 
 class TestGetValues(unittest.TestCase):
-    pass
+    def setUp(self) -> None:
+        with open("fixtures.json") as test_data:
+            [self.param1, self.param2, self.expected_result_format] = json.load(
+                test_data
+            )
+        self.empty_param = {"data": {"attributes": {"list": []}}}
+        return super().setUp()
+
+    def create_expected_result_dictionary(self, result_value):
+        expected_result = dict(self.expected_result_format)
+        expected_result["data"]["attributes"]["result"]["value"] = str(result_value)
+        return expected_result
+
+    def test_returns_correct_result_result_for_empty_param(self):
+        """Test that the function returns correct format and values for an empty list of parameters"""
+        actual_result = get_values(data=self.empty_param)
+        expected_result = self.create_expected_result_dictionary(0)
+        self.assertDictEqual(actual_result, expected_result)
+
+    def test_returns_correct_result_result_for_param1(self):
+        """Test that the function returns correct format and values for a list of two parameter pairs"""
+        actual_result = get_values(data=self.empty_param)
+        expected_result = self.create_expected_result_dictionary(0.4962 + 0.4810)
+        self.assertDictEqual(actual_result, expected_result)
+
+    def test_returns_correct_result_result_for_param2(self):
+        """Test that the function returns correct format and values for a list of one parameter pair"""
+        actual_result = get_values(data=self.empty_param)
+        expected_result = self.create_expected_result_dictionary(0.5062)
+        self.assertDictEqual(actual_result, expected_result)
 
 
 if __name__ == "__main__":
