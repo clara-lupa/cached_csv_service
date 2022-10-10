27d7b7c9adddfdff6aae65a68d6477c3283afacb
cw <cwo@systemli.org>
2022-10-10 16:54:39 +0200

    refactored caching_tests script and improved output

--- caching_tests.py
+++ caching_tests.py
@@ -1,21 +1,25 @@
 from main import get_values, get_value_for_data_points
 import timeit
 
+ATTRIBUTES_LIST_LENGTH = 100
+RUNS_PER_REPETITION = 10000
+REPETITIONS = 5
 
 param = {
     "data": {
         "attributes": {
             "list": [
                 {"power": "12", "date": "2019-01-22"},
-                {"power": "8", "date": "2019-02-22"},
             ]
-            * 100
+            * ATTRIBUTES_LIST_LENGTH
         }
     }
 }
 
 
-print(timeit.repeat(lambda: get_values(param), number=10000, repeat=5))
+print(
+    f"The minimum time for all repetitions was {min(timeit.repeat(lambda: get_values(param), number=RUNS_PER_REPETITION, repeat=REPETITIONS))}"
+)
 
 print(get_value_for_data_points.cache_info())
 get_value_for_data_points.cache_clear()
